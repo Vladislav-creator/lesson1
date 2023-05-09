95e5a4e51ffb8bebb5d3afd29f52f23c176f99c2
Vladislav-creator <valdisbeketov@gmail.com>
2023-05-09 15:53:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,429 @@
+//let a = 10
+//if(true){
+    //console.log(a)
+//}
+
+//let a = 25
+//if(true){
+    //let a = 5
+    //a = 35
+
+//}
+//console.log(a)
+
+//let a = 50
+//if(true){
+    //let a = 5
+    //a = 35
+    //if(false){
+   // let a = 15   
+   // a = 25
+   // console.log(a)
+    //}
+    //console.log(a) 
+//}
+//console.log(a)
+
+//a = 10
+//let a = 5
+//console.log(a)//Error
+//let a = 6
+//if(true){
+    //a = 7
+//if(false){
+    //a = 8
+//}
+//}
+//console.log(a)
+
+//let a = 7
+//console.log(a)
+//if(true){
+    //a = 6
+    
+//}
+//if(true){
+    //let a = 8
+    //a = 5
+//}
+    //let a = 15
+   // a = 44
+    //if(true){
+        //a = 25
+    //}
+    //console.log(a)
+
+    //function iAm(a){
+    //let Vlad 
+    //if(a === 7){
+        //Vlad = 'chempion'
+    //}else if(a === 3){
+        //Vlad = 'itproger'
+    //}else{
+        //Vlad = 'undefined'
+    //}
+    //return Vlad
+    //}
+    //console.log(iAm(7))
+
+    //let b = 35
+    //if(true){
+        //let a = 16
+        //b = 44
+    //if(true){
+        //a = 45
+        //b = 6
+    //}
+    //a = 77
+    //console.log(a)
+    //}
+    //console.log(b)
+
+    //let a = 16
+    //let b = 35
+    //if(true){
+        //b = 44
+    //if(true){
+        //a = 45
+        //b = 6
+    //}
+    //a = 77
+    //console.log(a)
+    //}
+    //console.log(b)
+
+    //let a = 16
+    //let b = 35
+    //if(true){
+        //a = 4
+        //let b = 35
+        //b = 44
+    //if(true){
+        //a = 45
+        //if(true){
+            //a = 99
+            //b = 0
+        //}
+        //b = 6
+    //}
+    //console.log(b)
+    //console.log(a)
+
+    //for(let i = 0; i < 5; i+=1){
+        //console.log(i);
+    //}
+    //const str ='hello world';
+    //for(let i = 0; i < str.length; i+=1){
+        //console.log(str[i]);
+    //}
+
+    //const str ='hello world';
+    //let i = 0;
+    //while (i < str.length){
+        //if(str[i] === 'w'){
+           // console.log('yes', [i]);
+            //break;
+        //}
+        //console.log('no', str[i]);
+        //i+=1;
+    //}
+
+    //const str ='hello world';
+    //let i = 0;
+    //do{
+        //if(str[i] === 'w'){
+        //console.log('w',[i]);
+        //break;
+        //}
+        //i+=1;
+    //}
+    //while(i<str.length);
+
+    //const str ='hello world w';
+    //for(i = 0; i < str.length; i+=1){
+        //if(str[i] === 'w'){
+        //console.log('yes',[i]);
+        //}else {
+            //console.log('no',[i]) 
+        //}
+    //}
+
+    
+    //let value = prompt('Whats the official name JavaScript?');
+    //value = value.toLowerCase();
+    //if (value === 'ecmascript'){
+       //alert('YES');
+    //}else{
+        //alert('no');
+    //}
+
+    //let value = Number(prompt('Введите число'));
+    //console.log(typeof value)
+    //if (value > 0 ){
+        //console.log('это позитивное число');
+    //}else if(value < 0){
+        //console.log('это негативное число');
+    //}else{
+        //console.log('это - 0');
+    //}
+
+    //let value = Number(prompt('Введите число'));
+    //if(!(value%2)){
+        //console.log('парное число');
+    //}else{
+        //console.log('не парное число');
+    //}
+
+    //let value = Number(prompt('Введите число'));
+    
+    //if(!(value%10)){
+        //console.log('Это число делится на 10 без остатка')
+    //}else{
+        //console.log('Последняя цифра введеного числа = '+ value%10);
+    //}
+
+    //let result
+    //const a = 120
+    //const b = 180
+    //if(a > 100 && b > 100){
+        //if(a > b){
+            //console.log(result = a)
+        //}else{
+            //console.log(result = b)
+        //}
+    //}else{
+        //console.log(result = b + 512)
+    //}
+
+    //const a = 1
+    //const b = 180
+    //if(a > 100 && b > 100){
+        //console.log(a > b? a: b)
+    //}else{
+        //console.log(b + 512)
+    //}
+
+    //const link = 'Vlad/.';
+    //if(link.includes('/.')){
+        //console.log('да, есть');
+    //}else{
+        //console.log(link +'/.');
+    //}
+
+    //let link = 'my-sitehttps://somesite.com/about'
+    //if(link.includes('my-site')&&!link.endsWith('/')){
+        //console.log(link+='/')
+    //}else{
+        //console.log('no')
+    //}
+
+    //let link = 'https://somesite.com/aboutmy-site';
+    //console.log(link.includes('my-site')&&!link.endsWith('/') ? link+='/': link);
+
+    //const hours = 23;
+    //if(hours < 17){
+        //console.log('Pending');
+    //}else if(hours <= 24){
+        //console.log('Expires');
+    //}else{
+       //console.log('Overdue')
+    //}
+
+    //const day = 2;
+    //switch(day){
+        //case 0:
+            //console.log('today');
+            //break;
+        //case 1:
+            //console.log('tomorrow');
+            //break;
+        //case 2:
+            //console.log('the day after tomorrow');
+            //break;
+        //default:
+            //console.log('day in the future');
+    //}
+    //function diaPason(min, max){
+    //let i;
+    //for(  i = min; i >= min && i < max ; i+=1){
+       //if(!(i%5)){
+        //console.log(i);
+       //};
+    //}; 
+    //return i;
+    //} ;
+    //console.log(diaPason(10, 20));
+    
+    //const login = prompt('Enter the login')
+    //if(!login){
+        //alert('Abolition');
+    //}else if(login === 'admin'){
+        //const password = prompt('Enter the password')
+        //alert(password === 'i admin'?'Good day':'Wrong password');
+    //}else {
+        //alert('i dont know you')
+    //}
+
+    //const value = prompt('Привет Лера!!! Как твои дела?');
+    //if (value === 'хорошо'||value === 'отлично'||value ==='нормально'){
+        //alert('Молодец Симпатяжка!!!')
+        //const value = prompt('Как твои тренировки?')
+        //alert(value === 'норма'||value === 'супер'?'Умничка':'Плохо');
+        //const value1 = prompt('Когда соревнования ближайшие ?');
+        //alert(value1 === 'скоро'||value1 === 'в этом месяце'?'желаю победить!!!':'значит планово готовся');
+    //}else if(!value){
+        //console.log('отмена');
+    //}else{
+        //alert('не правильный ответ')
+    //}
+
+    //const value = 5;
+    //if(value < 6){
+        //const value = 7;
+        //val = value + 1;
+    //}else{
+        //console.log(value > 6)
+    //}console.log(value)   
+    //console.log(typeof(8));
+    //console.log(typeof('8'));
+    //let element = '500,59';
+    //element = element.replace(',','.');
+    //console.log(typeof(Math.round(element)));
+    //console.log (Number(element).toFixed());
+    //element = Number(element).toFixed(1);
+    //console.log(element);
+    //let aliBaba = prompt('How old are you?');
+    //aliBaba = Number.parseInt(aliBaba);
+    //console.log(typeof aliBaba);
+    //console.log(aliBaba);
+    //let angeleDavids = 'BMW777';
+    //console.log(Number(angeleDavids));
+    //console.log(5/0);
+
+    //let value = prompt('Введите любое целое число a');
+    //value = Number(value);
+    //if (value){
+        //let value1 = prompt('Введите степень b для числа a');
+        //value1 = Number(value1);
+        //const result = value**value1;
+        //alert(value + '**' + value1 +' = '+ result);
+    //}else{
+        //console.log('отмена');
+    //};
+    //const min = 5
+    //const max = 99
+    //const result = Math.round(Math.random()*(max - min) + min)
+    //console.log(result);
+
+    //const Beketovi = ['Vladimir','Alexandr','Vladislav','Galina','Valeriya','Monika'];
+    //const min = 0;
+    //const max = Beketovi.length - 1;
+    //const index = Math.round(Math.random()*(max - min) + min);
+    //const Beketov = Beketovi[index];
+    //console.log(Beketov);
+
+    //message = 'В этой строке 24 символа';
+    //const lengthMessage = message.length;
+    //console.log(lengthMessage);
+    //let a = '8' + 7;
+    //console.log(typeof(a), a);
+    //let brand = 'Sumsung';
+    //brand = brand.toLowerCase();
+    //console.log(brand);
+    
+    //if(true){
+        //console.log(a)
+        //const a = 5
+        
+    //}
+
+    //const option = 'o';
+    //let mesage;
+    //switch(option){
+        //case 1:
+        //mesage = 'самовывоз';
+        //break;  
+        //case 2:
+       //mesage = 'курьер';
+        //break; 
+        //case 3:
+        //mesage = 'почта';
+        //break;
+        //default:
+        //mesage = 'вам перезвонит менеджер' 
+    //}
+    //console.log(option,mesage)
+    
+    //const min = 7;
+    //const max = 19
+    //let i;
+    //for(  i = min; i >= min && i <= max ; i+=1){
+        //if(!(i%7)){
+           //console.log(i);
+    //};
+    //}; 
+
+    //const minWages = 500;
+    //const maxWages = 100;
+    //const colEmployees = 7;
+    //let totalWages = 0;
+    //for(i = 1; i <= 7; i+=1){
+        //const wages = Math.round(Math.random()*(maxWages - minWages),
+         //+ minWages);
+         //console.log(`Зарплата ${i} сотрудника ${wages}`);
+        //totalWages+=wages
+    //}
+    //console.log("Зарплата всех сотрудников "+ totalWages); 
+    
+    //const min = 0;
+    //const max = 10;
+    //let sumChet = 0;
+    //let sumNochet = 0;
+    //for (i = min; i <= max; i +=1){
+        //if(!(i%2)){
+            //console.log("четное число " +i);
+            //sumChet += i;
+           //continue;
+        //}
+        //console.log( "нечетное число "+i);
+        //sumNochet +=i;
+        //}
+    //console.log( "Сумма четных чисел", sumChet );  
+    //console.log( "Сумма нечетных чисел", sumNochet );  
+    
+    //const min = 0;
+    //const max = 10;
+    //let sumChet = 0;
+    //let sumNochet = 0;
+    //for (i = min; i <= max; i +=1){
+        //if(i%2!==0){
+            //console.log("нечетное число " +i);
+            //sumNochet += i;
+           //continue;
+        //}
+        //console.log( "нечетное число "+i);
+        //sumChet +=i;
+        //}
+    //console.log( "Сумма четных чисел", sumChet );  
+    //console.log( "Сумма нечетных чисел", sumNochet );  
+
+
+
+
+    
+
+    
+
+
+
+   
+    
+    
+
+    
+
+    
+
+    
+    
+    
